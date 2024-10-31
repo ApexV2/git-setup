181d90be753fb2aa439d623247c5577d409f757f
ApexV2 <vojtechklep@gmail.com>
2024-10-31 19:06:40 +0100

    Add command-line arguments and documentation

--- bin/git-setup.js
+++ bin/git-setup.js
@@ -4,6 +4,26 @@ const inquirer = require('inquirer');
 const { execSync } = require('child_process');
 const chalk = require('chalk');
 
+// Documentation text
+const shortDocs = `
+${chalk.bold('Git Setup CLI')} - Quick repository setup tool
+
+${chalk.yellow('Usage:')}
+  git-setup -s    Start the setup process
+  git-setup -d    Show this documentation
+
+${chalk.yellow('Example:')}
+  git-setup -s
+  > What is the remote name? (origin)
+  > What is the remote repository URL? https://github.com/username/repo.git
+  > What is your main branch name? (main)
+  > Initialize a new repository? (Y/n)
+
+${chalk.yellow('Requirements:')}
+  - Git installed on your system
+  - Valid repository URL
+`;
+
 async function setupGitRepo() {
     try {
         const answers = await inquirer.prompt([
@@ -56,4 +76,29 @@ async function setupGitRepo() {
     }
 }
 
-setupGitRepo(); 
+// Main function to handle command line arguments
+function main() {
+    const args = process.argv.slice(2);
+    
+    if (args.length === 0 || args[0] === '--help' || args[0] === '-h') {
+        console.log(chalk.red('Error: Missing required argument\n'));
+        console.log(shortDocs);
+        process.exit(1);
+    }
+
+    switch (args[0]) {
+        case '-s':
+            setupGitRepo();
+            break;
+        case '-d':
+            console.log(shortDocs);
+            break;
+        default:
+            console.log(chalk.red('Error: Invalid argument\n'));
+            console.log(shortDocs);
+            process.exit(1);
+    }
+}
+
+// Run the program
+main(); 
