326f0a336c82e865bdd3943707671c37bffa44ca
Viktor Skarda <viktor.skarda@clevertify.com>
2024-11-03 01:50:07 +0100

    Add prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import eslint from '@eslint/js';
+import tslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tslint.config(eslint.configs.recommended, ...tslint.configs.recommended, {
+    languageOptions: {
+        globals: {
+            ...globals.node,
+        },
+    },
+});
